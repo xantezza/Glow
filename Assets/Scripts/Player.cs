1b55d3483a0edc8b186e60c1f7a4d06516e89859
nirasv <itori.ohm@gmail.com>
2021-05-16 21:33:30 +0300

    division of responsibility by scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
-
-public class Player : MonoBehaviour
+using System.Collections;public class Player : MonoBehaviour
 {
 	[SerializeField] private Rigidbody _rigidbody;
+	[SerializeField] private AudioSource audioSource;
 	private Vector2 moveDirection;
-	private bool isAbleToMove = false;
-
-	[SerializeField] private UIController uIController;
-
+	private bool isAbleToMove = true;
 
 	void Start()
     {
 		_rigidbody.Sleep();
 	}
 
-    public void Launch()
-    {
-		isAbleToMove = true;
-	}
+   
 	void FixedUpdate()
     {
 		if (isAbleToMove)
@@ -35,13 +28,22 @@ public class Player : MonoBehaviour
 			), ForceMode.Acceleration);
 		}
 	}
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("PlayerKiller"))
-        {
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("PlayerKiller"))
+		{
 			isAbleToMove = false;
 			_rigidbody.Sleep();
-			uIController.PlayerDying();
+			StartCoroutine(PlayerDied());
 		}
-    }
+	}
+	private IEnumerator PlayerDied()
+	{
+			for (float i = 1; i > 0; i -= 0.01f)
+			{
+				audioSource.pitch = i;
+				yield return new WaitForSecondsRealtime(0.02f);
+			}
+			SceneManager.LoadSceneAsync("Start");
+	}
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,31 +1,9 @@
 using UnityEngine;
-using System.Collections;
-
+using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private Player playerScript;
-    [SerializeField] private GameObject button;
-	[SerializeField] private AudioSource audioSource;
 	public void Launch()
     {
-		playerScript.Launch();
-		button.SetActive(false);
-		audioSource.Play();
-	}
-
-	public void PlayerDying()
-	{
-		StartCoroutine(SomeCoroutine());
-		
-	}
-
-	private IEnumerator SomeCoroutine()
-	{
-		for (float i = 1; i > 0; i -= 0.01f)
-		{
-			audioSource.pitch = i;
-			yield return new WaitForSecondsRealtime(0.02f);
-		}
-
+		SceneManager.LoadSceneAsync("Main");
 	}
 }
